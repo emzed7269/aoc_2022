642461a99a5630376461e9e099632cdde5ea586d
emzed7269 <78227232+emzed7269@users.noreply.github.com>
2022-12-01 14:15:59 +0100

    second star done

--- day1.py
+++ day1.py
@@ -9,6 +9,22 @@ day1puzzle = day1data("day1input.txt")
 
 
 def first_star(puzzle: list) -> int:
+    elf_carriage = 0
+    current_elf = 0
+    for n in puzzle:
+        if n != '':
+            current_elf += int(n)
+        else:
+            if current_elf > elf_carriage:
+                elf_carriage = current_elf
+            current_elf = 0
+    return elf_carriage
+
+
+first_star(day1puzzle)
+
+
+def second_star(puzzle: list) -> int:
     elf_carriage = []
     current_elf = 0
     for n in puzzle:
@@ -17,8 +33,10 @@ def first_star(puzzle: list) -> int:
         else:
             elf_carriage.append(current_elf)
             current_elf = 0
-    return max(elf_carriage)
+    elf_carriage.sort(reverse=True)
+    aggregate_bigboys = sum(elf_carriage[:3])
+    return aggregate_bigboys
 
 
-first_star(day1puzzle)
-
+second_star(day1puzzle)
+print(second_star(day1puzzle))
