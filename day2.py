32356946cf9b3dec5c3c18654863bf6e5711e28e
emzed7269 <78227232+emzed7269@users.noreply.github.com>
2022-12-03 23:28:43 +0100

    day 2

--- day2.py
+++ day2.py
@@ -10,5 +10,27 @@ def day2data(filename: str) -> list:
 
 
 day2puzzle = day2data('day2input.txt')
+print(day2puzzle)
 
-print(day2data("day2input.txt"))
+
+def first_star(puzzle: list) -> list:
+    move_score = {'X': 1, 'Y': 2, 'Z': 3}
+    move_equivalence = {'A': 'X', 'B': 'Y', 'C': 'Z'}
+    move_wins = {'X': 'Y', 'Y': 'Z', 'Z': 'X'}
+    total_score = 0
+    for n in range(len(puzzle)):
+        if puzzle[n] in move_equivalence:
+            le_move = puzzle[n]
+            puzzle[n] = move_equivalence[le_move]
+        if n % 2 == 1:
+            if puzzle[n] == puzzle[n-1]:
+                total_score += move_score[puzzle[n]] + 3
+            elif puzzle[n] == move_wins[puzzle[n-1]]:
+                total_score += move_score[puzzle[n]] + 6
+            else:
+                total_score += move_score[puzzle[n]]
+    return total_score
+
+
+print(first_star(day2puzzle))
+
